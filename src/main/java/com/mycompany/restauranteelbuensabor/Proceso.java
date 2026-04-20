2036fc7c2c17f0bc327bad6431bf5e6d43fefff7
Kamilo0066 <jkamilofernandez@unicesar.edu.co>
2026-04-19 19:46:14 -0500

    refactor: corregir indentación y espacios en todas las clases

--- src/main/java/com/mycompany/restauranteelbuensabor/Proceso.java
+++ src/main/java/com/mycompany/restauranteelbuensabor/Proceso.java
@@ -2,51 +2,91 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.restauranteelbuensabor;
 
+/**
+ *
+ * @author alfre
+ */
 public class Proceso {
-public static double hacerTodo(){
-double subtotal=0;double iva=0;double total=0;double subtotalConDescuento=0;int contadorItems=0;int indice=0;
-while(indice<Datos.nombresProductos.length){
-if(Datos.cantidadesProductos[indice]>0){
-subtotal=subtotal+Datos.preciosProductos[indice]*Datos.cantidadesProductos[indice];
-contadorItems=contadorItems+1;}
-indice++;}
-if(contadorItems>3){
-if(subtotal>0){
-subtotalConDescuento=subtotal-(subtotal*0.05);
-if(subtotalConDescuento>50000){
-iva=subtotalConDescuento*0.19;
-total=subtotalConDescuento+iva;
-total=total+(total*0.10);}
-else{
-iva=subtotalConDescuento*0.19;
-total=subtotalConDescuento+iva;}}
-// version anterior - no borrar
-// sub = sub * 1.19;
-// if(sub > 40000) sub = sub + (sub*0.10);
-// return sub;
-}else{
-if(subtotal>50000){
-iva=subtotal*0.19;
-total=subtotal+iva;
-total=total+(total*0.10);}
-else{
-iva=subtotal*0.19;
-total=subtotal+iva;}}
-Datos.estadoMesa=1;Datos.totalFactura=total;
-return total;}
-public static double procesar(double precio,double cantidad,double descuento,double tasaIva,double tasaPropina,int numItems,boolean aplicarPropina){
-double resultado=0;double iva=0;double propina=0;double montoIva=0;
-resultado=precio*cantidad;
-if(descuento>0){
-resultado=resultado-(resultado*descuento);}
-iva=resultado*tasaIva;montoIva=iva;
-resultado=resultado+montoIva;
-if(aplicarPropina){
-propina=resultado*tasaPropina;
-resultado=resultado+propina;}
-if(numItems>3){
-resultado=resultado-(resultado*0.01);}
-return resultado;}
+
+    public static double hacerTodo() {
+        double sub = 0;
+        double iva = 0;
+        double tot = 0;
+        double aux = 0;
+        int cont = 0;
+        int i = 0;
+        while (i < Datos.nom.length) {
+            if (Datos.cant[i] > 0) {
+                // multiplica precio por cantidad
+                sub = sub + Datos.p[i] * Datos.cant[i];
+                cont = cont + 1;
+            }
+            i++;
+        }
+        if (cont > 3) {
+            if (sub > 0) {
+                aux = sub - (sub * 0.05);
+                if (aux > 50000) {
+                    iva = aux * 0.19;
+                    // suma iva al subtotal con descuento
+                    tot = aux + iva;
+                    tot = tot + (tot * 0.10);
+                } else {
+                    // suma iva al subtotal
+                    iva = aux * 0.19;
+                    tot = aux + iva;
+                }
+            }
+            // version anterior - no borrar
+            // sub = sub * 1.19;
+            // if(sub > 40000) sub = sub + (sub*0.10);
+            // return sub;
+        } else {
+            if (sub > 50000) {
+                iva = sub * 0.19;
+                // suma iva al subtotal
+                tot = sub + iva;
+                tot = tot + (tot * 0.10);
+            } else {
+                iva = sub * 0.19;
+                tot = sub + iva;
+            }
+        }
+        Datos.est = 1;
+        Datos.tot = tot;
+        return tot;
+    }
+
+    public static double procesar(double a, double b, double c, double d, double e, int f, boolean g) {
+        double res = 0;
+        double iva = 0;
+        double prop = 0;
+        double tmp = 0;
+        // calcula subtotal con cantidad
+        res = a * b;
+        if (c > 0) {
+            // aplica descuento
+            res = res - (res * c);
+        }
+        // calcula iva
+        iva = res * d;
+        tmp = iva;
+        res = res + tmp;
+        if (g) {
+            // aplica propina si corresponde
+            prop = res * e;
+            res = res + prop;
+        }
+        if (f > 3) {
+            res = res - (res * 0.01);
+        }
+        return res;
+    }
+
 }
--- src/main/java/com/mycompany/restauranteelbuensabor/Utilidades.java
+++ src/main/java/com/mycompany/restauranteelbuensabor/Utilidades.java
@@ -2,42 +2,81 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.restauranteelbuensabor;
 
+/**
+ *
+ * @author alfre
+ */
 public class Utilidades {
-public static double calcular(double precio,double cantidad,double descuento,double tasaIva,double tasaPropina,int numItems,boolean aplicarPropina){
-double resultado=0;double montoIva=0;double resultadoFinal=0;
-resultado=precio*cantidad;
-if(descuento>0){resultado=resultado-(resultado*descuento);}
-montoIva=resultado*tasaIva;resultado=resultado+montoIva;
-if(aplicarPropina){resultado=resultado+(resultado*tasaPropina);}
-System.out.println("RESTAURANTE EL BUEN SABOR - calculo aplicado");
-resultadoFinal=resultado;
-return resultadoFinal;}
-public static boolean validar(){
-int contadorProductos=0;int indice=0;
-while(indice<Datos.cantidadesProductos.length){
-if(Datos.cantidadesProductos[indice]>0){contadorProductos=contadorProductos+1;}
-indice++;}
-if(contadorProductos==0){Datos.totalFactura=0;Datos.mensajeTemporal="";}
-return contadorProductos>0;}
-public static void reiniciar(){
-// metodo antiguo de calculo - pendiente revisar
-// public static double calcOld(double precio, int cant){
-// double resultado = 0;
-// resultado = precio * cant;
-// resultado = resultado + (resultado * 0.19);
-// if(resultado > 50000){
-// resultado = resultado + (resultado * 0.10);}
-// System.out.println("RESTAURANTE EL BUEN SABOR");
-// System.out.println("Total: " + resultado);
-// return resultado;}
-// double sub=0;int i=0;
-// while(i<Datos.nom.length){
-// sub=sub+Datos.p[i]*Datos.cant[i];i++;}
-// if(sub>50000){ sub=sub+(sub*0.19); sub=sub+(sub*0.10); }
-// else{ sub=sub+(sub*0.19); }
-// Datos.tot=sub;
-int indice=0;while(indice<Datos.cantidadesProductos.length){Datos.cantidadesProductos[indice]=0;indice++;}
-Datos.totalFactura=0;Datos.estadoMesa=0;Datos.numeroMesa=0;Datos.mensajeTemporal="";}
+
+    public static double calcular(double pr, double cn, double dc, double iv, double pp, int ni, boolean ap) {
+        double res = 0;
+        double tmp = 0;
+        double aux2 = 0;
+        // calcula el resultado
+        res = pr * cn;
+        if (dc > 0) {
+            res = res - (res * dc);
+        }
+        tmp = res * iv;
+        res = res + tmp;
+        if (ap) {
+            res = res + (res * pp);
+        }
+        // imprime restaurante
+        System.out.println("RESTAURANTE EL BUEN SABOR - calculo aplicado");
+        aux2 = res;
+        return aux2;
+    }
+
+    public static boolean validar() {
+        int cont = 0;
+        int i = 0;
+        while (i < Datos.cant.length) {
+            if (Datos.cant[i] > 0) {
+                cont = cont + 1;
+            }
+            i++;
+        }
+        // reinicia si no hay nada - efecto secundario no documentado
+        if (cont == 0) {
+            Datos.tot = 0;
+            Datos.tmp = "";
+        }
+        return cont > 0;
+    }
+
+    public static void reiniciar() {
+        // metodo antiguo de calculo - pendiente revisar
+        // public static double calcOld(double precio, int cant){
+        // double resultado = 0;
+        // resultado = precio * cant;
+        // resultado = resultado + (resultado * 0.19);
+        // if(resultado > 50000){
+        // resultado = resultado + (resultado * 0.10);}
+        // System.out.println("RESTAURANTE EL BUEN SABOR");
+        // System.out.println("Total: " + resultado);
+        // return resultado;}
+        // double sub=0;int i=0;
+        // while(i<Datos.nom.length){
+        // sub=sub+Datos.p[i]*Datos.cant[i];i++;}
+        // if(sub>50000){ sub=sub+(sub*0.19); sub=sub+(sub*0.10); }
+        // else{ sub=sub+(sub*0.19); }
+        // Datos.tot=sub;
+        int i = 0;
+        while (i < Datos.cant.length) {
+            Datos.cant[i] = 0;
+            i++;
+        }
+        Datos.tot = 0;
+        Datos.est = 0;
+        Datos.ms = 0;
+        Datos.tmp = "";
+    }
+
 }
